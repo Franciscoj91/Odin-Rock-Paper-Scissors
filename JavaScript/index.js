79b619e164975fdc971be93c0f9ebe2000d698fb
francisco <franjmarin1991@gmail.com>
2023-05-01 17:16:33 -0400

    Create playRound function
    This function defines if it's a tie, you win or lose the round

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -12,3 +12,23 @@ function getComputerChoice() {
             break;
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "It's a tie!!!"
+    }else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You lose! Paper beats Rock";
+    }else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats Scissors";
+    }else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock";
+    }else if (playerSelection === "paper" && computerSelection === "scissors") {
+        return "You lose! Scissors beats Paper";
+    }else if (playerSelection === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors";
+    }else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats Paper";
+    }
+}
